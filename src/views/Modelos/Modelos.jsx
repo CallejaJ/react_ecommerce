8522c967f4ced18019511ca5a2d8c436ee68133e
Your Name <you@example.com>
2023-06-17 14:25:09 +0200

    react-bootstap

--- src/views/Modelos/Modelos.jsx
+++ src/views/Modelos/Modelos.jsx
@@ -7,50 +7,55 @@ import slider2 from '../../assets/slider2.jpg'
 import slider3 from '../../assets/slider3.jpg'
 import { Link } from "react-router-dom"
 import "../../CSS/style.css"
-
+import Carousel from 'react-bootstrap/Carousel';
 
 function Modelos() {
     return (
         <>
-            <div id="carouselExampleCaptions" className="carousel slide">
-                <div className="carousel-indicators">
-                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                </div>
-                <div className="carousel-inner">
-                    <div className="carousel-item active">
-                        <img src={slider1} className="d-block w-100" alt="slider1" />
-                        <div className="carousel-caption d-none d-md-block">
-                            <h5 className="h1 text-primary">Modelos 3D</h5>
-                            <p className="h4 text-primary">a la vanguardia</p>
 
-                        </div>
-                    </div>
-                    <div className="carousel-item">
-                        <img src={slider2} className="d-block w-100" alt="slider2" />
-                        <div className="carousel-caption d-none d-md-block">
-                            <h5 className="h1 text-primary">Set 2</h5>
-                            <p className="h4 text-primary">Diseño personalizado</p>
-                        </div>
-                    </div>
-                    <div className="carousel-item">
-                        <img src={slider3} className="d-block w-100" alt="maqueta3" />
-                        <div className="carousel-caption d-none d-md-block">
-                            <h5 className="h1 text-primary">Set 3</h5>
-                            <p className="h4 text-primary">Construimos tus sueños</p>
-                        </div>
-                    </div>
-                </div>
-                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Previous</span>
-                </button>
-                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Next</span>
-                </button>
-            </div>
+            <Carousel>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={slider1}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h3>First slide label</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={slider2}
+                        alt="Second slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Second slide label</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={slider3}
+                        alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Third slide label</h3>
+                        <p>
+                            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                        </p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+
+
+
+
 
 
             <Card cards={card} />

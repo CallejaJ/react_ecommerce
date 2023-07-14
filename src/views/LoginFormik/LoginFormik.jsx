b96433fb9ebb777ef14fd943e6bd8d64386c8aef
Your Name <you@example.com>
2023-07-14 10:21:14 +0200

    LoginFormik

---
+++ src/views/LoginFormik/LoginFormik.jsx
@@ -0,0 +1,24 @@
+import { Formik } from "formik";
+import { LoginFormikView } from "../LoginFormik/LoginFormikView";
+import { LoginFormikSchema } from "../LoginFormik/LoginFormikSchema";
+import { initialValues } from "../LoginFormik/utils/form";
+import { useAuthContext } from "../../context/AuthContext";
+
+export default function LoginFormik() {
+    const { login } = useAuthContext();
+
+    function onSubmit(values, actions) {
+        login(values);
+        actions.resetForm();
+    }
+
+    return (
+        <Formik
+            initialValues={initialValues}
+            validationSchema={LoginFormikSchema}
+            onSubmit={onSubmit}
+        >
+            {(props) => <LoginFormikView formik={props} />}
+        </Formik>
+    );
+}

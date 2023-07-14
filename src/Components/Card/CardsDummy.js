7407b04b070fc4952bfa96fee019dcccab6576b4
Your Name <you@example.com>
2023-07-14 18:28:28 +0200

    prices

--- src/Components/Card/CardsDummy.js
+++ src/Components/Card/CardsDummy.js
@@ -13,81 +13,81 @@ import modelo11 from '../../assets/webp/modelo11.webp'
 
 
 export const card = [
-    {
-      titulo: "Modelo 1",
-      precio: "7.355",
-      imagen: modelo1,
-      texto: "Vivienda de 200 m2",
-      id: 1
-    },
-    {
-      titulo: "Modelo 2",
-      precio: "6.895",
-      imagen: modelo2,
-      texto: "vivienda de 270 m2",
-      id:2
-    },
-    {
-      titulo: "Modelo 3",
-      precio: "5.995",
-      imagen: modelo3,
-      texto: "Vivienda de 260 m2",
-      id:3
-    },
-    {
-      titulo: "Modelo 4",
-      precio: "8.795",
-      imagen: modelo4,
-      texto: "Vivienda de 300 m2",
-      id:4
-    },
-    {
-      titulo: "Modelo 5",
-      precio: "8.695",
-      imagen: modelo5,
-      texto: "Vivienda de 320 m2",
-      id:5
-    },
-    {
-      titulo: "Modelo 6",
-      precio: "7.595",
-      imagen: modelo6,
-      texto: "Vivienda de 520 m2",
-      id:6
-    } ,
-    {
-      titulo: "Modelo 7",
-      precio: "18.395",
-      imagen: modelo7,
-      texto: "Vivienda de 620 m2",
-      id:7
-    } ,
-    {
-      titulo: "Modelo 8",
-      precio: "8.395",
-      imagen: modelo8,
-      texto: "Vivienda de 320 m2",
-      id:8
-    } ,
-    {
-      titulo: "Modelo 9",
-      precio: "7.395",
-      imagen: modelo9,
-      texto: "Vivienda de 620 m2",
-      id:9
-    } ,
-    {
-      titulo: "Modelo 10",
-      precio: "6.395",
-      imagen: modelo10,
-      texto: "Vivienda de 420 m2",
-      id:10
-    } ,
-    {
-      titulo: "Modelo 11",
-      precio: "6.395",
-      imagen: modelo11,
-      texto: "Vivienda de 520 m2",
-      id:11
-    }     
-  ]
+  {
+    titulo: "Modelo 1",
+    precio: "167.355",
+    imagen: modelo1,
+    texto: "Vivienda de 200 m2",
+    id: 1
+  },
+  {
+    titulo: "Modelo 2",
+    precio: "216.895",
+    imagen: modelo2,
+    texto: "vivienda de 270 m2",
+    id: 2
+  },
+  {
+    titulo: "Modelo 3",
+    precio: "205.995",
+    imagen: modelo3,
+    texto: "Vivienda de 260 m2",
+    id: 3
+  },
+  {
+    titulo: "Modelo 4",
+    precio: "348.795",
+    imagen: modelo4,
+    texto: "Vivienda de 300 m2",
+    id: 4
+  },
+  {
+    titulo: "Modelo 5",
+    precio: "278.695",
+    imagen: modelo5,
+    texto: "Vivienda de 320 m2",
+    id: 5
+  },
+  {
+    titulo: "Modelo 6",
+    precio: "417.595",
+    imagen: modelo6,
+    texto: "Vivienda de 520 m2",
+    id: 6
+  },
+  {
+    titulo: "Modelo 7",
+    precio: "518.395",
+    imagen: modelo7,
+    texto: "Vivienda de 620 m2",
+    id: 7
+  },
+  {
+    titulo: "Modelo 8",
+    precio: "418.395",
+    imagen: modelo8,
+    texto: "Vivienda de 320 m2",
+    id: 8
+  },
+  {
+    titulo: "Modelo 9",
+    precio: "317.395",
+    imagen: modelo9,
+    texto: "Vivienda de 420 m2",
+    id: 9
+  },
+  {
+    titulo: "Modelo 10",
+    precio: "196.395",
+    imagen: modelo10,
+    texto: "Vivienda de 220 m2",
+    id: 10
+  },
+  {
+    titulo: "Modelo 11",
+    precio: "426.395",
+    imagen: modelo11,
+    texto: "Vivienda de 320 m2",
+    id: 11
+  }
+]
